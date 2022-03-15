732bf17c6fec571a2a5bfb43a5c70f307c35e7ec
Yarik <yarik747@yandex.ru>
2022-03-15 23:51:17 +0300

    fix code 15 03 22

---
+++ src/components/Main/MyPosts/MyPostsContainer.jsx
@@ -0,0 +1,30 @@
+/** @format */
+
+import React from "react";
+// import Post from "./Post/Post";
+import {
+  addPostActionCreator,
+  updateNewPostTextActionCreator,
+} from "../../../redux/profile-reducer";
+import MyPosts from "./MyPosts";
+
+const MyPostsContainer = (props) => {
+  let addPost = () => {
+    props.store.dispatch(addPostActionCreator());
+  };
+
+  let onPostChange = (text) => {
+    let action = updateNewPostTextActionCreator(text);
+    props.store.dispatch(action);
+  };
+
+  return (
+    <MyPosts
+      updateNewPostText={onPostChange}
+      addPost={addPost}
+      posts={props.posts}
+    />
+  );
+};
+
+export default MyPostsContainer;
